8e5b5b8cd83f4cc37701825a5cea6f0ef9d3bc5f
Kaylee Flanary <kflanary@glenrosetigers.info>
2018-12-14 14:24:36 -0600

    worked on function__construct

--- VideoHandler.php
+++ VideoHandler.php
@@ -8,18 +8,18 @@
 class VideoHandler
 {
 
-Private $id // the ID of the file in the database
-Private $video_file //directory location of the converted video
-Private $gif_file //directory location of the converted gif
-Private $still_file // directory location of the still frame
-Private $video_file_url // url of the converted video
-Private $gif_file_url // url of the converted gif
-Private $still_file_url //url of the still frame
-Private $width // the x resolution of the video
-Private $height // the y resolution of the video
-Private $duration // length of the video
-Private $ffmpeg // instance of the ffmpeg class to perform operations
-Private $db // instance of the Connect class that will handle SQL Queries
+Private $id; // the ID of the file in the database
+Private $video_file; //directory location of the converted video
+Private $gif_file; //directory location of the converted gif
+Private $still_file; // directory location of the still frame
+Private $video_file_url; // url of the converted video
+Private $gif_file_url; // url of the converted gif
+Private $still_file_url; //url of the still frame
+Private $width; // the x resolution of the video
+Private $height; // the y resolution of the video
+Private $duration; // length of the video
+Private $ffmpeg; // instance of the ffmpeg class to perform operations
+Private $db; // instance of the Connect class that will handle SQL Queries
 
     
     /**
@@ -29,7 +29,8 @@ Private $db // instance of the Connect class that will handle SQL Queries
     public function __construct()
     {
         
-        // TODO - Insert your constructor code here
+      $this->width=1080;
+      $this->height=720;
     }
     
     /**
